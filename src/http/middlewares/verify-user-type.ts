aef038f2f9055957617e8ee171423a8098444088
Matheus Henrique <matheushenrique_cunha@hotmail.com>
2025-07-15 23:28:46 -0300

    refactor: simplify user type extraction in verifyUserRole middleware

--- src/http/middlewares/verify-user-type.ts
+++ src/http/middlewares/verify-user-type.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export function verifyUserRole(roleToVerify: 'admin' | 'member') {
   return async (request: FastifyRequest, reply: FastifyReply) => {
-    const { user_type } = request.user as { user_type: string }
+    const { user_type } = request.user
     if (user_type !== roleToVerify) throw reply.status(401).send({ message: 'Não autorizado.' })
   }
 }
